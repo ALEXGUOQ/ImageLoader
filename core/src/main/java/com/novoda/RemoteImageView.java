f0a8d910f225928349ca97c7371c2458da2a8b74
Ataul Munim <timberwo7ves@gmail.com>
2014-01-20 12:19:34 +0000

    Adds autoloading RemoteView (loads URL)

--- core/src/main/java/com/novoda/RemoteImageView.java
+++ core/src/main/java/com/novoda/RemoteImageView.java
@@ -2,45 +2,43 @@ package com.novoda;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.novoda.imageloader.R;
+import com.novoda.imageloader.core.ImageManager;
+import com.novoda.imageloader.core.LoaderSettings;
+import com.novoda.imageloader.core.model.ImageTagFactory;
 
 public class RemoteImageView extends FrameLayout {
 
-    private ImageView imageView;
+    private final ImageView imageView;
+
     private String url;
 
-    public RemoteImageView(Context context) {
-        super(context);
-    }
-
     public RemoteImageView(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
+        super(context, attrs);
         setLayoutParams(new LayoutParams(context, attrs));
+        obtainStyledAttributes(attrs);
+
+        this.imageView = new ImageView(context);
     }
 
     public RemoteImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         setLayoutParams(new LayoutParams(context, attrs));
         obtainStyledAttributes(attrs);
+
+        this.imageView = new ImageView(context);
     }
 
     @Override
     protected void onFinishInflate() {
-        super.onFinishInflate();
-        addImageView();
-
-        Toast.makeText(getContext(), "url: " + url, Toast.LENGTH_SHORT).show();
-    }
-
-    private void addImageView() {
-        imageView = new ImageView(getContext());
         addView(imageView, getLayoutParams());
         imageView.setBackgroundColor(getResources().getColor(android.R.color.black));
+        load();
     }
 
     private void obtainStyledAttributes(AttributeSet attrs) {
@@ -56,6 +54,21 @@ public class RemoteImageView extends FrameLayout {
             }
         }
         typedArray.recycle();
+    }
+
+    private void load(){
+        if (TextUtils.isEmpty(url)) {
+            return;
+        }
+
+        LoaderSettings settings = new LoaderSettings.SettingsBuilder().build(getContext());
+        ImageManager imageLoader = new ImageManager(settings);
+        ImageTagFactory tagFactory = ImageTagFactory.newInstance();
+        tagFactory.setWidth(100);
+        tagFactory.setHeight(100);
+        imageView.setTag(tagFactory.build(url, getContext()));
+
+        imageLoader.getLoader().load(imageView);
     }
 
 }
--- demo/src/main/java/com/novoda/imageloader/demo/activity/RemoteImageViewActivity.java
+++ demo/src/main/java/com/novoda/imageloader/demo/activity/RemoteImageViewActivity.java
@@ -3,6 +3,7 @@ package com.novoda.imageloader.demo.activity;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.novoda.RemoteImageView;
 import com.novoda.imageloader.demo.R;
 
 public class RemoteImageViewActivity extends Activity {
@@ -11,6 +12,8 @@ public class RemoteImageViewActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote_image_view);
+
+        RemoteImageView view = (RemoteImageView) findViewById(R.id.remote_image_view);
     }
 
 }
