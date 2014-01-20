e70915132fe85209c58e16a5ff876ed28806ea5d
Ataul Munim <timberwo7ves@gmail.com>
2014-01-20 13:51:26 +0000

    Adds cached_ dimens attributes for ImageTag width height, with defaults

--- core/src/main/java/com/novoda/RemoteImageView.java
+++ core/src/main/java/com/novoda/RemoteImageView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
@@ -14,40 +15,57 @@ import com.novoda.imageloader.core.model.ImageTagFactory;
 
 public class RemoteImageView extends FrameLayout {
 
-    private final ImageView imageView;
+    private static final int DEFAULT_TAG_DIMENSION = 1024;
 
+    private ImageView imageView;
     private String url;
+    private int tagWidth = DEFAULT_TAG_DIMENSION;
+    private int tagHeight = DEFAULT_TAG_DIMENSION;
+    private ImageManager imageLoader;
+
+
+    public RemoteImageView(Context context) {
+        super(context);
+        initRemoteView(context);
+        load();
+    }
 
     public RemoteImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setLayoutParams(new LayoutParams(context, attrs));
-        obtainStyledAttributes(attrs);
-
-        this.imageView = new ImageView(context);
+        initRemoteView(context);
+        initFromAttributes(context, attrs);
+        load();
     }
 
     public RemoteImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        setLayoutParams(new LayoutParams(context, attrs));
-        obtainStyledAttributes(attrs);
-
-        this.imageView = new ImageView(context);
-    }
-
-    @Override
-    protected void onFinishInflate() {
-        addView(imageView, getLayoutParams());
-        imageView.setBackgroundColor(getResources().getColor(android.R.color.black));
+        initRemoteView(context);
+        initFromAttributes(context, attrs);
         load();
     }
 
-    private void obtainStyledAttributes(AttributeSet attrs) {
-        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.RemoteImageView);
+    private void initRemoteView(Context context) {
+        this.imageView = new ImageView(context);
+        setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        addView(imageView, getLayoutParams());
+
+        LoaderSettings settings = new LoaderSettings.SettingsBuilder().build(getContext());
+        imageLoader = new ImageManager(settings);
+    }
+
+    private void initFromAttributes(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.RemoteImageView);
         for (int i = 0; i < typedArray.getIndexCount(); i++) {
             int attr = typedArray.getIndex(i);
             switch (attr) {
                 case R.styleable.RemoteImageView_url:
                     url = typedArray.getString(attr);
+                    break;
+                case R.styleable.RemoteImageView_cached_width:
+                    tagWidth = typedArray.getInt(attr, DEFAULT_TAG_DIMENSION);
+                    break;
+                case R.styleable.RemoteImageView_cached_height:
+                    tagHeight = typedArray.getInt(attr, DEFAULT_TAG_DIMENSION);
                     break;
                 default:
                     break;
@@ -56,16 +74,19 @@ public class RemoteImageView extends FrameLayout {
         typedArray.recycle();
     }
 
-    private void load(){
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void load(){
         if (TextUtils.isEmpty(url)) {
+            Log.w(getClass().getSimpleName(), "No url has been set on this view; cannot load image.");
             return;
         }
 
-        LoaderSettings settings = new LoaderSettings.SettingsBuilder().build(getContext());
-        ImageManager imageLoader = new ImageManager(settings);
         ImageTagFactory tagFactory = ImageTagFactory.newInstance();
-        tagFactory.setWidth(100);
-        tagFactory.setHeight(100);
+        tagFactory.setWidth(tagWidth);
+        tagFactory.setHeight(tagHeight);
         imageView.setTag(tagFactory.build(url, getContext()));
 
         imageLoader.getLoader().load(imageView);
